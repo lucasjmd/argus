e689ff37bf446071f37ef2af9aea2f817cd89abf
lucasjmd <lucas.deijkers@proton.me>
2026-02-02 21:50:54 +0100

    tests: added integration test stream_sim with test csv file

--- libraries/sentinel-core/tests/test_base.py
+++ libraries/sentinel-core/tests/test_base.py
@@ -1,5 +1,6 @@
 import pytest
-from sentinel.domain.base import BaseIngestor, CSVIngestor, stream_simulator
+from sentinel.domain.base import BaseIngestor, CSVIngestor, stream_simulator, StreamIngestor
+from pathlib import Path
 
 def test_cannot_instantiate_abc():
     with pytest.raises(TypeError) as e:
@@ -12,3 +13,20 @@ def test_stream_sim_type_raise():
     with pytest.raises(TypeError) as e:
         generator_obj = stream_simulator('test.xlsx')
         next(generator_obj)
+
+def test_streamingestor_type_raise():
+    with pytest.raises(TypeError):
+        stream = StreamIngestor('test.xlsx')
+        next(stream)
+
+def test_stream_sim_data_read(tmp_path):
+    unit_test_data_dir = tmp_path / 'data'
+    unit_test_data_dir.mkdir()
+    test_filestring = unit_test_data_dir / 'test_tx_data.csv'
+    test_filestring.write_text('1, PAYMENT, 9839.64, C1231006815, 170136.0, 160296.36, M1979787155, 0.0, 0.0, 0, 0')
+
+    gen_obj = stream_simulator(str(test_filestring))
+
+    row_data = next(gen_obj)
+    assert row_data == ['1, PAYMENT, 9839.64, C1231006815, 170136.0, 160296.36, M1979787155, 0.0, 0.0, 0, 0']
+
