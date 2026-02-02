ffafa9f3eca4a76e57519ce2e7d59bd41b6b171c
lucasjmd <lucas.deijkers@proton.me>
2026-02-02 18:33:36 +0100

    feat: add simple stream simulator of csv

--- libraries/sentinel-core/src/sentinel/domain/base.py
+++ libraries/sentinel-core/src/sentinel/domain/base.py
@@ -61,14 +61,49 @@ class CSVIngestor(BaseIngestor):
             print("Encountered an error in reading the batch.")
             return False
 
+class StreamIngestor(BaseIngestor):
+
+    def __init__(self, data_source):
+        self.data_source = data_source
+
+    def __enter__(self):
+        print("Connecting to stream...")
+        self.data_obj = open(f'{PAYSIM_DIR}/{self.data_source}', 'r')
+        self.reader_obj = csv.reader(self.data_obj)
+        return self
+
+    def get_transactions(self):
+        for row in self.reader_obj:
+            yield row
+
+    def __exit__(self, exc_type, exc_value, exc_traceback):
+        if exc_type is StopIteration:
+            print('Reached the end of the batch.')
+            self.data_obj.close()
+            return True
+        else:
+            self.data_obj.close()
+            print("Encountered an error in reading the batch.")
+            return False
 
 
-# class StreamIngestor(BaseIngestor):
+def stream_simulator(data):
+    while True:
+        data_obj = open(f'{PAYSIM_DIR}/{data}', 'r')
+        reader_obj = csv.reader(data_obj)
 
-CSV_object = CSVIngestor('paysim_dataset.csv')
+        for row in reader_obj:
+            yield row
+
+
 
 if __name__ == '__main__':
-    with CSVIngestor('paysim_dataset.csv') as data:
-        for transaction in data.get_transactions():
-            print(transaction)
+    # with CSVIngestor('paysim_dataset.csv') as data:
+    #     for transaction in data.get_transactions():
+    #         print(transaction)
+
+    stream = stream_simulator('paysim_dataset.csv')
+
+    for tx in stream:
+        print(tx)
 
