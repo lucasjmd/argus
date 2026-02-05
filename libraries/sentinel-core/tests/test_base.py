75152e19fe2a7cea69f8a85cf07473f27d449321
lucasjmd <lucas.deijkers@proton.me>
2026-02-05 19:56:19 +0100

    fix: integration test of row content

--- libraries/sentinel-core/tests/test_base.py
+++ libraries/sentinel-core/tests/test_base.py
@@ -2,14 +2,23 @@ import pytest
 from sentinel.domain.base import BaseIngestor, CSVIngestor, stream_simulator, StreamIngestor
 from pathlib import Path
 
-def test_cannot_instantiate_abc():
-    with pytest.raises(TypeError) as e:
-        BaseIngestor()
+## UNIT TESTS
+
+# Correct instantiation
 
 def test_instantiate_csvingestor():
-    CSVIngestor(1)
+    CSVIngestor('abc')
 
-def test_stream_sim_type_raise():
+def test_instant_csvingestor_wrongtype():
+    with pytest.raises(TypeError) as e:
+        CSVIngestor(1)
+
+def test_instant_streamsim():
+    stream_simulator('abc.csv')
+
+# Incorrect argument type
+
+def test_stream_sim_data_type_raise():
     with pytest.raises(TypeError) as e:
         generator_obj = stream_simulator('test.xlsx')
         next(generator_obj)
@@ -19,14 +28,22 @@ def test_streamingestor_type_raise():
         stream = StreamIngestor('test.xlsx')
         next(stream)
 
+def test_cannot_instantiate_abc():
+    with pytest.raises(TypeError) as e:
+        BaseIngestor()
+
+
+## INTEGRATION TESTS
+
 def test_stream_sim_data_read(tmp_path):
+    row_content = '1,PAYMENT,9839.64,C1231006815,170136.0,160296.36,M1979787155,0.0,0.0,0,0'
     unit_test_data_dir = tmp_path / 'data'
     unit_test_data_dir.mkdir()
     test_filestring = unit_test_data_dir / 'test_tx_data.csv'
-    test_filestring.write_text('1, PAYMENT, 9839.64, C1231006815, 170136.0, 160296.36, M1979787155, 0.0, 0.0, 0, 0')
+    test_filestring.write_text(row_content)
 
     gen_obj = stream_simulator(str(test_filestring))
 
     row_data = next(gen_obj)
-    assert row_data == ['1, PAYMENT, 9839.64, C1231006815, 170136.0, 160296.36, M1979787155, 0.0, 0.0, 0, 0']
+    assert row_data == ['1', 'PAYMENT', '9839.64', 'C1231006815', '170136.0', '160296.36', 'M1979787155', '0.0', '0.0', '0', '0']
 
