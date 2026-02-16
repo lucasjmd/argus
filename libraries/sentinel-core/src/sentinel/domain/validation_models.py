d5de67c33629cc8151be9b910764d9290df76e38
lucasjmd <lucas.deijkers@proton.me>
2026-02-16 20:46:36 +0100

    fix: add keyword dict for pydantic class call

--- libraries/sentinel-core/src/sentinel/domain/validation_models.py
+++ libraries/sentinel-core/src/sentinel/domain/validation_models.py
@@ -2,8 +2,23 @@ from pydantic import BaseModel, condecimal
 from decimal import Decimal
 
 # constrained schema for balance-related columns
+
+pydantic_keyword_dict = {
+    'step':None,
+    'type': None,
+    'amount': None,
+    'nameOrig': None,
+    'oldbalanceOrg': None,
+    'newbalanceOrig': None,
+    'nameDest': None,
+    'oldbalanceDest': None,
+    'newbalanceDest': None,
+    'isFraud': None,
+    'isFlaggedFraud':None,
+}
+
 balance_schema = condecimal(
-        strict=True,
+        strict=False,
         ge=0,
         decimal_places=2,
         allow_inf_nan=False,
@@ -13,7 +28,7 @@ class Transaction(BaseModel):
     step: int
     type: str
     amount: condecimal(
-        strict=True,
+        strict=False,
         gt=0,
         decimal_places=2,
         allow_inf_nan=False,
@@ -26,6 +41,7 @@ class Transaction(BaseModel):
     newbalanceDest: balance_schema
     isFraud: int
     isFlaggedFraud: int
+
 
 
 
