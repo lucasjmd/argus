fd02c7f3e8bd7e5479782fbe8e66a72c53a55ef3
lucasjmd <lucas.deijkers@proton.me>
2026-02-16 20:49:52 +0100

    refactor: remove unnecessary temp data from pydantic validation test file

--- libraries/sentinel-core/tests/test_validation_models.py
+++ libraries/sentinel-core/tests/test_validation_models.py
@@ -6,17 +6,6 @@ from pathlib import Path
 from decimal import Decimal
 
 
-header_sample = 'step, type, amount, nameOrig, oldbalanceOrg, newbalanceOrig, nameDest, oldbalanceDest, newbalanceDest,\
- isFraud, isFlaggedFraud'
-row_sample_1 = ['1','PAYMENT','9839.64','C1231006815','170136.0','160296.36','M1979787155','0.0','0.0','0,0']
-row_sample_2 = ['2','TRANSFER','1234.56','C840083671','1234567.1','89101112.13','M408069119','1.0','0.2','3','4']
-
-PAYSIM_DIR = '/home/lucas/github/argus/data'
-base_dir = Path(PAYSIM_DIR)
-filestring = Path('paysim_dataset.csv')
-full_path = base_dir / filestring
-
-
 def test_simple_row_pass():
     with CSVIngestor(full_path) as data:
         results = list(itertools.islice(data.get_transactions(), 1))
