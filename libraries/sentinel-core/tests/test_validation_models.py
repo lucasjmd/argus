e953a900801870b506c449bec4be0377ec833867
lucasjmd <lucas.deijkers@proton.me>
2026-02-20 15:24:35 +0100

    test: added unit test for missing and incorrect type fields for pydantic validation model

--- libraries/sentinel-core/tests/test_validation_models.py
+++ libraries/sentinel-core/tests/test_validation_models.py
@@ -1,22 +1,35 @@
 import pytest
+from pydantic import ValidationError
 from sentinel.domain.validation_models import Transaction, pydantic_keyword_dict
-from sentinel.domain.base import CSVIngestor, StreamIngestor
+from sentinel.domain.base import CSVIngestor, StreamIngestor, full_path
 import itertools
 from pathlib import Path
 from decimal import Decimal
 
+with CSVIngestor(full_path) as data:
+    results = list(itertools.islice(data.get_transactions(), 1))
+
+    i = 0
+    for key in pydantic_keyword_dict.keys():
+        pydantic_keyword_dict[key] = results[0][i]
+        i += 1
+
+missing_keyword_dict = pydantic_keyword_dict.copy()
+missing_keyword_dict['oldbalanceOrg'] = None
+
+wrongtype_keyword_dict = pydantic_keyword_dict.copy()
+wrongtype_keyword_dict['step'] = 'shouldBeInt'
+
+def test_missing_field():
+    with pytest.raises(ValidationError) as e:
+        tx = Transaction(**missing_keyword_dict)
+
+def test_incorrect_type():
+    with pytest.raises(ValidationError) as e:
+        tx = Transaction(**wrongtype_keyword_dict)
 
 def test_simple_row_pass():
-    with CSVIngestor(full_path) as data:
-        results = list(itertools.islice(data.get_transactions(), 1))
-
-        i = 0
-        for key in pydantic_keyword_dict.keys():
-            pydantic_keyword_dict[key] = results[0][i]
-            i += 1
-
     tx = Transaction(**pydantic_keyword_dict)
-
 
     assert isinstance(tx.step, int)
     assert isinstance(tx.type, str)
