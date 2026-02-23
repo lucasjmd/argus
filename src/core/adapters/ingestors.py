592156313b24a22aa92029cd33f02522a91ceb96
lucasjmd <lucas.deijkers@proton.me>
2026-02-23 20:05:03 +0100

    refactor: moved concrete ingestors to adapters dir

---
+++ src/core/adapters/ingestors.py
@@ -0,0 +1,124 @@
+class CSVIngestor(BaseIngestor):
+    """
+    Concrete ingestor engine class for ingesting 'batch'  transaction data coming from a .csv file.
+
+    This concrete version of the BaseIngestor abstract base class specifies how batch or static data has to be
+    ingested. It can be used as a context manager and offers a graceful file exit. The data is presented to the caller
+    via a generator object that can be iterated through.
+
+    Args:
+        data_source (str): The csv file representing the batch or static data.
+
+    Attributes:
+        None
+
+    """
+
+    def __init__(self, data_source: str):
+        self.data_source = data_source
+
+        base_dir = Path(PAYSIM_DIR)
+        filestring = Path(data_source)
+        self.full_path = base_dir / filestring
+
+    def __enter__(self):
+        print("Connecting to batch...")
+        self.file_obj = open(f'{self.full_path}', 'r')
+        return self
+
+    def get_transactions(self) -> Generator[list[str], None, None]:
+        """
+        Generator that yields each row of the csv.
+        It defines no .send method or finishing return statement.
+
+        Yields:
+            List[str]: A generator object that can iterate through the rows of the batch/static data.
+        """
+        self.batch_obj = csv.reader(self.file_obj)
+
+        # Skip header
+        next(self.batch_obj, None)
+
+        for row in self.batch_obj:
+            time.sleep(0.2)
+            yield row
+
+    def __exit__(self, exc_type, exc_value, exc_traceback):
+        if exc_type is StopIteration:
+            print('Reached the end of the batch.')
+            self.file_obj.close()
+            return True
+        else:
+            self.file_obj.close()
+            print("Encountered an error in reading the batch.")
+            return False
+
+
+class StreamIngestor(BaseIngestor):
+    """
+    Concrete ingestor engine class for ingesting stream transaction data.
+
+    This concrete version of the BaseIngestor abstract base class specifies how stream data has to be
+    ingested. It can be used as a context manager. The data is presented to the caller via an infinite generator
+    object that can be iterated through.
+
+    Args:
+        data_source (str): The connection representing the stream of data.
+
+    Attributes:
+        None
+
+    """
+    def __init__(self, data_source: str, throttle: bool = True):
+        self.data_source    = data_source
+        self.throttle       = throttle
+
+    def __enter__(self):
+        print("Connecting to stream...")
+        # This is until we connect to a real stream
+        self.stream_obj = stream_simulator(f'{self.data_source}')
+
+        #Checking if stream has any data.
+        try:
+            first_value = next(self.stream_obj)
+            stream_obj = itertools.chain(first_value, self.stream_obj)
+        except StopIteration:
+            print('Stream is empty.')
+
+        return self
+
+    def get_transactions(self) -> Generator[list[str], None, None]:
+        """
+        Generator that yields each row of a stream of data.
+        It defines no .send method or finishing return statement.
+
+        Yields:
+            List[str]: A generator object that can iterate through the rows of the stream data.
+        """
+        for row in self.stream_obj:
+            if self.throttle:
+                time.sleep(0.2)
+            yield row
+
+    #TODO: Add a graceful file close down.
+    def __exit__(self, exc_type, exc_value, exc_traceback):
+        if exc_type is StopIteration:
+            print('The stream has been interrupted.')
+            self.stream_obj.close()
+            return True
+
+        else:
+            print("Encountered an error in reading the batch.")
+            self.stream_obj.close()
+            return False
+
+
+
+if __name__ == '__main__':
+    with CSVIngestor('paysim_dataset.csv') as data:
+        for transaction in data.get_transactions():
+            print(transaction)
+
+    # with StreamIngestor('paysim_dataset.csv', True) as data:
+    #     for tx in data.get_transactions():
+    #         print(tx)
