f4af45907cbadefe7fb4d2feb0f65a1373cfbcc5
lucasjmd <lucas.deijkers@proton.me>
2026-02-24 20:32:27 +0100

    refactor: clean up base.py with linter suggestions

--- src/core/domain/base.py
+++ src/core/domain/base.py
@@ -6,8 +6,9 @@ class BaseIngestor(ABC):
     Abstract base class for ingesting transaction data in various forms.
 
     This is the base class for various database connection subclasses.
-    It is meant as a contract that defines the form concrete classes (the specific source ingestors)
-    will have to follow. It is also set up to allow it to be used as a custom context manager.
+    It is meant as a contract that defines the form concrete classes
+    (the specific source ingestors) will have to follow. It is also
+    set up to allow it to be used as a custom context manager.
 
     Args:
         None
