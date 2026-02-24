947eca97a599d3885c87a3e17d801abb6c4909dd
lucasjmd <lucas.deijkers@proton.me>
2026-02-24 20:29:53 +0100

    refactor: clean up code lint suggestions

--- src/core/adapters/ingestors.py
+++ src/core/adapters/ingestors.py
@@ -1,14 +1,12 @@
-from typing import Generator
 import csv
-import time
-from pathlib import Path
-import sys
 import itertools
+import sys
+import time
+from collections.abc import Generator
+from pathlib import Path
 
-
-from core.domain.base import BaseIngestor
 from core.adapters.stream_simulator import stream_simulator
-
+from core.domain.base import BaseIngestor
 
 stream_simulator_dir = Path(__file__).resolve().parents[3] / 'tests'
 sys.path.append(str(stream_simulator_dir))
@@ -21,10 +19,12 @@ full_path = base_dir / filestring
 
 class CSVIngestor(BaseIngestor):
     """
-    Concrete ingestor engine class for ingesting 'batch'  transaction data coming from a .csv file.
+    Concrete ingestor engine class for ingesting 'batch'  transaction data
+    coming from a .csv file.
 
-    This concrete version of the BaseIngestor abstract base class specifies how batch or static data has to be
-    ingested. It can be used as a context manager and offers a graceful file exit. The data is presented to the caller
+    This concrete version of the BaseIngestor abstract base class specifies
+    how batch or static data has to be ingested. It can be used as a context
+    manager and offers a graceful file exit. The data is presented to the caller
     via a generator object that can be iterated through.
 
     Args:
@@ -44,7 +44,7 @@ class CSVIngestor(BaseIngestor):
 
     def __enter__(self):
         print("Connecting to batch...")
-        self.file_obj = open(f'{self.full_path}', 'r')
+        self.file_obj = Path.open(f'{self.full_path}', 'r')
         return self
 
     def get_transactions(self) -> Generator[list[str], None, None]:
@@ -53,7 +53,8 @@ class CSVIngestor(BaseIngestor):
         It defines no .send method or finishing return statement.
 
         Yields:
-            List[str]: A generator object that can iterate through the rows of the batch/static data.
+            List[str]: A generator object that can iterate through the rows of
+            the batch/static data.
         """
         self.batch_obj = csv.reader(self.file_obj)
 
@@ -69,19 +70,19 @@ class CSVIngestor(BaseIngestor):
             print('Reached the end of the batch.')
             self.file_obj.close()
             return True
-        else:
-            self.file_obj.close()
-            print("Encountered an error in reading the batch.")
-            return False
 
+        self.file_obj.close()
+        print("Encountered an error in reading the batch.")
+        return False
 
 class StreamIngestor(BaseIngestor):
     """
     Concrete ingestor engine class for ingesting stream transaction data.
 
-    This concrete version of the BaseIngestor abstract base class specifies how stream data has to be
-    ingested. It can be used as a context manager. The data is presented to the caller via an infinite generator
-    object that can be iterated through.
+    This concrete version of the BaseIngestor abstract base class specifies how
+    stream data has to be ingested. It can be used as a context manager.
+    The data is presented to the caller via an infinite generator object that
+    can be iterated through.
 
     Args:
         data_source (str): The connection representing the stream of data.
@@ -114,7 +115,8 @@ class StreamIngestor(BaseIngestor):
         It defines no .send method or finishing return statement.
 
         Yields:
-            List[str]: A generator object that can iterate through the rows of the stream data.
+            List[str]: A generator object that can iterate through the rows of
+            the stream data.
         """
         for row in self.stream_obj:
             if self.throttle:
@@ -128,10 +130,10 @@ class StreamIngestor(BaseIngestor):
             self.stream_obj.close()
             return True
 
-        else:
-            print("Encountered an error in reading the batch.")
-            self.stream_obj.close()
-            return False
+        print("Encountered an error in reading the batch.")
+        self.stream_obj.close()
+        return False
+
 
 
 if __name__ == '__main__':
