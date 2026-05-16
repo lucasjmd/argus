6241d5f81d80ac54408812fd58cd901322a0aed4
lucasjmd <lucas.deijkers@proton.me>
2026-05-16 17:49:17 +0200

    docs: add sql set up script

---
+++ init-scripts/01-init.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE IF NOT EXISTS paysim;
+USE paysim;
+
+CREATE TABLE IF NOT EXISTS transactions (
+    step INT,
+    type VARCHAR(20),
+    amount DECIMAL(15,2),
+    nameOrig VARCHAR(20),
+    oldbalanceOrg DECIMAL(15,2),
+    newbalanceOrig DECIMAL(15,2),
+    nameDest VARCHAR(20),
+    oldbalanceDest DECIMAL(15,2),
+    newbalanceDest DECIMAL(15,2),
+    isFraud TINYINT(1),
+    isFlaggedFraud TINYINT(1),
+);
