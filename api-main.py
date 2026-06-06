930c64d29a96e70f30db128caaa338abb868eab8
lucasjmd <lucas.deijkers@proton.me>
2026-06-06 16:34:11 +0200

    feat: expose transactions in various forms via fastapi

---
+++ api-main.py
@@ -0,0 +1,30 @@
+from fastapi import FastAPI, HTTPException
+from src.core.adapters.databases import MySQLTransactions
+import pandas as pd
+
+app = FastAPI()
+
+@app.get('/')
+def root():
+    return {'Hello': 'World!'}
+
+@app.get('/transactions')
+def get_sample_tx_json() -> dict:
+
+    return MySQLTransactions.get_sample_transactions()
+
+@app.get('/transactions/amount/{value}')
+def get_tx_gte_amount_json(value: float) -> dict:
+
+    return MySQLTransactions.get_transactions_above_amount(value)
+
+@app.get('/transactions/orig/{account_id}')
+def get_tx_orig_account_json(account_id: str) -> dict:
+
+    return MySQLTransactions.get_transactions_orig_account(account_id)
+
+@app.get('/transactions/dest/{account_id}')
+def get_tx_dest_account_json(account_id: str) -> dict:
+    return MySQLTransactions.get_transactions_dest_account(account_id)
+
+#TODO: Add get route for fraudulent tx
