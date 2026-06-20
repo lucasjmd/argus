8ed37c395a32cd849a1d51c6a3f98e78da04867d
lucasjmd <lucas.deijkers@proton.me>
2026-06-20 15:38:27 +0200

    feat: jwt token signer

---
+++ api_token_signer.py
@@ -0,0 +1,20 @@
+from jose import jwt
+from dotenv import load_dotenv
+import os
+import pytz
+
+load_dotenv()
+
+secret = os.getenv('JWT_SECRET')
+expire = os.genenv('JWT_EXPIRY_SECONDS')
+algo = os.getenv('JWT_ALGORITHM')
+
+def create_jwt(data: dict) -> str:
+
+    expiry_dt = datetime.now(pytz.timezone('Europe/Amsterdam'))
+
+    data['exp'] = expiry_dt
+
+    token = jwt.encode(data, secret, algorithm=algo)
+
+    return token
