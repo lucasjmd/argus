102a954003f0c2cb5a90f63f99f5496f06f624cc
lucasjmd <lucas.deijkers@proton.me>
2026-06-20 14:57:02 +0200

    feat: validation of incoming pw attempt to stored pw

--- auth_hashing.py
+++ auth_hashing.py
@@ -1,8 +1,27 @@
 import bcrypt
 
 def hash_user_password(password: str) -> str:
+    """
+    Hashes incoming api user passwords for secure storage in database
 
+    :param password: str The user's plain-text password.
+    :return: str User's hashed password
+    """
     bytes = password.encode('utf-8')
     hashed= bcrypt.hashpw(bytes, bcrypt.gensalt())
 
     return hashed
+
+def validate_password_attempt(password_attempt: str, stored_password: str) -> bool:
+    """
+    Compares login attempt password with hashed password
+
+    :param
+        - password_attempt: str The user's password attempt
+        - stored_password: str The user's stored password
+
+    :return: bool True if passwords match, False otherwise
+    """
+    bytes = password.encode('utf-8')
+    return bcrypt.checkpw(bytes, stored_password)
+
