f70e05a94fd26db3eaf8b79940fe07d56a98c056
lucasjmd <lucas.deijkers@proton.me>
2026-06-23 19:31:57 +0200

    feat: protect routes

--- src/api/routes.py
+++ src/api/routes.py
@@ -1,36 +1,38 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Depends
 from src.core.adapters.databases import MySQLTransactions
+from src.api.auth.authenticator import validate_user
 from pydantic import BaseModel
 from typing import List, Dict
 
 app = FastAPI()
+
 db = MySQLTransactions()
 
 @app.get('/')
-def root():
+def root(user_id: str = Depends(validate_user)):
     return {'Hello': 'World!'}
 
 @app.get('/transactions')
-def get_txs_json(page: int=1, limit: int=50) -> list:
+def get_txs_json(page: int=1, limit: int=50, user_id: str = Depends(validate_user)) -> list:
 
     return db.get_transactions(page, limit)
 
 @app.get('/transactions/amount/{value}')
-def get_tx_gte_amount_json(value: float) -> list:
+def get_tx_gte_amount_json(value: float, user_id: str = Depends(validate_user)) -> list:
 
     return db.get_transactions_above_amount(value)
 
 @app.get('/transactions/orig/{account_id}')
-def get_tx_orig_account_json(account_id: str) -> list:
+def get_tx_orig_account_json(account_id: str, user_id: str = Depends(validate_user)) -> list:
 
     return db.get_transactions_orig_account(account_id)
 
 @app.get('/transactions/dest/{account_id}')
-def get_tx_dest_account_json(account_id: str) -> list:
+def get_tx_dest_account_json(account_id: str, user_id: str = Depends(validate_user)) -> list:
     return db.get_transactions_dest_account(account_id)
 
 @app.get('/transactions/summary/{account_id}')
-def get_sum_account_json(account_id: str) -> dict:
+def get_sum_account_json(account_id: str, user_id: str = Depends(validate_user)) -> dict:
     txs = db.get_transactions_orig_account(account_id)
 
     if not txs:
