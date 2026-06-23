2350d23a84ccc09a30a4a34fcfc2a5bd1fa6ecd9
lucasjmd <lucas.deijkers@proton.me>
2026-06-23 19:19:50 +0200

    feat: user authenticator and jwt token decoder

--- src/api/auth/authenticator.py
+++ src/api/auth/authenticator.py
@@ -2,7 +2,28 @@ from fastapi import Depends, HTTPException, status
 from jose import JWTError
 
 from fastapi.security import OAuth2PasswordBearer
-from core.auth import decode
+from api.auth.jwt_token_engine import decode_jwt
+
+# create token extractor
+oauth2_scheme = OAuth2PasswordBearer(tokenurl='login')
+
+
+def validate_user(token: str = Depends(oauth2_scheme)):
+    try:
+        payload = decode_jwt(token)
+        user_id = payload.get('sub')
+
+        if not user_id:
+            raise HTTPException(401, 'Invalid token')
+
+        return user_id
+
+    except JWTError:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail='Invalid or expired token'
+        )
+
 
 
 
